49b10b18aea989aa4ab1113b3ffbdb9cd69946ea
fortnite <anuwatch6552@gmail.com>
2023-01-10 15:26:39 +0700

    saved

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int gcd(int g = 0, int a = 0){
-    
-}
+int gcd(int k, int j);
+
 
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int k,int j){
+    if (k%j == 0){
+        return j;
+    }
+    else{
+        return gcd(j, k%j);
+    }
 }
