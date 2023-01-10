e75f4d429dccc7bfe67c51f6176d89264e6a4749
fortnite <anuwatch6552@gmail.com>
2023-01-10 14:59:22 +0700

    saved

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int h);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+
+int fibonacci(int h){
+    if (h <= 1) return h;
+    return fibonacci(h-1) + fibonacci(h-2);
+}
+
+
+
